0e40256fb700528d24c0d4e8026461c62abc5a12
carlosxino <90924523+carlosxino@users.noreply.github.com>
2021-09-19 19:10:51 -0500

    version inicial

---
+++ app/src/main/java/com/example/gaeliapp/otraActivity.java
@@ -0,0 +1,14 @@
+package com.example.gaeliapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otra);
+    }
+}
